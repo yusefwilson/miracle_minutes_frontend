0e44cd1fe1605cec32205b31d4ded12d30568df2
Seforius <yusefwilsonx@gmail.com>
2023-11-16 21:47:19 -0500

    Refactor newsletter

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -4,10 +4,10 @@ import { LoginContext } from "../App";
 import Dropdown from "./Dropdown";
 import axios from "axios";
 
-export default function Newsletters()
+export default function Articles()
 {
     const navigate = useNavigate();
-    const [newsletters, setNewsletters] = useState([]); // list of newsletters
+    const [articles, setArticles] = useState([]); // list of article
     const {loggedIn} = useContext(LoginContext);
     const [errorMessage, setErrorMessage] = useState(''); // error message
     
@@ -15,28 +15,28 @@ export default function Newsletters()
     {
         if(!loggedIn) { navigate('/login'); return;}
 
-        async function get_newsletters()
+        async function get_articles()
         {
-            // send access token and get newsletters
+            // send access token and get article
             const access_token = Cookies.get('miracle_minutes_access_token');
-            const newsletters_result = await axios.post('/newsletters', {access_token: access_token});
-            return newsletters_result.data;
+            const article_result = await axios.post('/articles', {access_token: access_token});
+            return article_result.data;
         }
-        const newsletter_result = get_newsletters();
+        const article_result = get_articles();
         
-        if(newsletter_result.error) { setErrorMessage(newsletter_result.error); return; }
-        setNewsletters(newsletter_result.newsletters);
+        if(article_result.error) { setErrorMessage(article_result.error); return; }
+        setArticles(article_result.articles);
 
     }, []);
 
 
-    //render error messages or newsletters
+    //render error messages or article
     return (
         <div className="flex flex-col justify-center bg-pink-300">
-            <h1>Newsletters</h1>
+            <h1>Articles</h1>
             {errorMessage === '' ?
             <div className="flex flex-col gap-y-2.5">
-                {newsletters.map((newsletter) => <Dropdown title={newsletter.title} content={newsletter.content}/>)}
+                {articles.map((newsletter) => <Dropdown title={newsletter.title} content={newsletter.content}/>)}
             </div>
             :
             <div className="flex flex-col gap-y-2.5">

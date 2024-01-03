e7fb41310da4aaa17936049f2717db1cb372e1b6
Yusef Wilson <55645544+Seforius@users.noreply.github.com>
2024-01-03 14:11:15 +1300

    Add datepicker to Articles

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -23,39 +23,46 @@ export default function Articles({set_current_component})
     const [articles, set_articles] = useState(null); // list of article
     const {logged_in} = useContext(LOGIN_CONTEXT);
     const [error_message, set_error_message] = useState(''); // error message
+    const [date, set_date] = useState(_get_utc_date());
+
+    const get_articles = async () =>
+    {
+        try
+        {
+            //send access token and get article
+            const access_token = Cookies.get('miracle_minutes_access_token');
+            
+            console.log('posting date ' + date);
+            const article_result = await axios.post('/articles', {date, access_token: access_token});
+            console.log(article_result.data.articles);
+            const articles = article_result.data.articles;
+            set_articles(articles);
+            console.log(articles);
+            set_error_message('');
+        }
+        catch (error)
+        {
+            console.log(error.response.data.error);
+            set_articles([]);
+            set_error_message(error.response.data.error);
+        }
+    }
     
     useEffect(() =>
     {
         if(!logged_in) { navigate('/login'); return;}
-
-        async function get_articles()
-        {
-            try
-            {
-                //send access token and get article
-                const access_token = Cookies.get('miracle_minutes_access_token');
-
-                const date = _get_utc_date();
-                console.log('date: ' + date);
-
-                const article_result = await axios.post('/articles', {date, access_token: access_token});
-                console.log(article_result.data.articles);
-                const articles = article_result.data.articles;
-                set_articles(articles);
-                console.log(articles);
-                set_error_message('');
-            }
-            catch (error)
-            {
-                console.log(error.response.data.error);
-                set_articles([]);
-                set_error_message(error.response.data.error);
-            }
-        }
         get_articles();
 
-    }, [logged_in, navigate]);
+    // eslint-disable-next-line
+    }, [logged_in, navigate, date]);
 
+    const handle_date_change = (event) =>
+    {
+        //convert date to yyyy-mm-dd format
+        const date = event.target.value;
+        console.log('handle date change with date: ' + date);
+        set_date(date);
+    }
 
     //render error messages or article
     return (
@@ -68,8 +75,9 @@ export default function Articles({set_current_component})
         :
 
         <div className='flex flex-col grid content-center justify-center bg-white w-full overflow-y-auto'>
-            <div className='bg-gray-400 rounded overflow-y-auto p-16 space-y-8'>
+            <div className='bg-gray-400 rounded overflow-y-auto p-16 space-y-8 flex flex-col justify-center'>
                 <h1 className='text-center text-5xl'>Articles</h1>
+                <input className='focus:outline-none border border-gray-300 p-2 rounded-md' type='date' value={date}onChange={handle_date_change}/>
                 <div className='flex flex-col space-y-8'>
                     {articles.map((article) => <Dropdown key={article.category} title={article.category} content={article.article}/>)}
                     {articles.length === 0 ? <div className='text-center text-white'>

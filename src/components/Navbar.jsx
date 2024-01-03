0534e5a608f42381bc35cbada3fef41136d42351
Yusef Wilson <55645544+Seforius@users.noreply.github.com>
2024-01-03 21:33:45 +1300

    Add logos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,13 @@ import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { LOGIN_CONTEXT } from '../App';
 import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
 
 export default function Navbar()
 {
   //get the login state and the function to set the login state from the context
   const {logged_in, set_logged_in} = useContext(LOGIN_CONTEXT);
+  const navigate = useNavigate();
   
   const button_style_string = 'hover:bg-gray-400 rounded p-4';
 
@@ -21,7 +23,7 @@ export default function Navbar()
   return (
     <div className='flex shrink-0 flex-row bg-purple-200 justify-between p-2 z-10'>
       <div className='flex items-center'>
-        <img src='/img/hourglass_purple_transparent.png' alt='Logo' width='64'/>
+        <img className='cursor-pointer' src='/img/hourglass_purple_transparent.png' alt='Logo' width='64' onClick={() => navigate('/')}/>
         <Link className={button_style_string} to='/'>Home</Link>
       </div>
       <div className='flex items-center'>

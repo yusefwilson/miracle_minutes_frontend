6303494ad9c9f08b9b5a16b2e1209a1ebd5e84d6
Yusef Wilson <55645544+Seforius@users.noreply.github.com>
2024-05-10 11:12:28 -0400

    Progress Plans; add PlanCard

---
+++ src/components/PlanCard.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+export default function PlanCard({ name, price, description, features, id }) {
+
+    const button_style_string = 'bg-purple-300 text-center hover:bg-black text-black font-bold py-2 px-4 border-2 border-black hover:border-transparent hover:text-white rounded-full cursor-pointer mx-2';
+
+    const onFreeTrial = () => {
+        console.log('Free trial started for plan: ', id);
+    }
+
+    return (
+        <div className='flex flex-col border-4 w-1/5 rounded-lg'>
+            <div className='flex flex-col grow p-8 gap-4'>
+                <h2 className='text-2xl font-bold'>{name}</h2>
+                <h3 className='text-xl font-bold'>${price + '/month'}</h3>
+                <p className='text-lg'>{description}</p>
+            </div>
+            <div className='flex flex-col p-8 gap-4'>
+                <ul>
+                    {features.map((feature, index) => <li key={index}>{'- ' + feature}</li>)}
+                </ul>
+                <Link className={button_style_string} to='/signup' onClick={onFreeTrial}>Start Free Trial!</Link>
+            </div>
+        </div>
+    );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ export default function Home()
             <div className='flex flex-col grid justify-items-center p-8 space-y-8'>
                 <h1 className='text-5xl text-center'>Save time, gain knowledge</h1>
                 <p className='text-center w-1/2'>Are you sick of spending hours watching poorly-edited, long-winded YouTube videos to find out information? Well worry no more, because with Miracle Minutes' AI-powered summarizing, you can receive concise, accurate summaries of important information that only take a few minutes to read!</p>
-                {logged_in ? <Link className={signup_button_style_string} to='/dashboard/shop'>Upgrade</Link> : <Link className={signup_button_style_string} to='/signup'>Start your free trial now!</Link>}
+                {logged_in ? <Link className={signup_button_style_string} to='/dashboard/shop'>Upgrade</Link> : <Link className={signup_button_style_string} to='/plans'>Start your free trial now!</Link>}
             </div>
         </div>
     );
